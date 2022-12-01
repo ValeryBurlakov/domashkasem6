937a82328dd6def6196c509369995bd104cb0fe8
Valery <wogl@mail.ru>
2022-12-02 02:07:26 +0300

    not complete

--- 41zadacha/Program.cs
+++ 41zadacha/Program.cs
@@ -3,27 +3,59 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
-// int t = 0;
-// int[] array = new int[t];
-// while (t < 1000)
-// {
-//     Console.WriteLine(t + 1);
-//     Array.Resize(ref array, t + 1);
-//     t++;
-// }
+Console.WriteLine("Здравствуйте. Будем считать количество чисел больше 0!");
+bool input = true;
+int count = 0;
+while (input == true)
+{
+    Console.WriteLine("Введите число:");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number > 0)
+    {
+        count = count + 1;
+        Repeat:
+        Console.WriteLine("Хотите ввести ещё число?(да/нет):");
+        string? answer = Console.ReadLine();
+        if (answer?.ToLower() == "да")
+        {
+        input = true;
+        }
+        else if (answer?.ToLower() == "нет")
+        {
+            Console.WriteLine($"Вы ввели чисел больше 0: {count}. Всего доброго!");
+            input = false;
+        }
+        else
+        {
+            Console.WriteLine("вы ввели ответ неправильно. Либо 'да' либо 'нет' !!!");
+            goto Repeat;
+        }
+    }
+    else 
+    {
+        Repeat2:
+        Console.WriteLine("Хотите ввести ещё число?(да/нет):");
+        string? answer = Console.ReadLine();
+        if (answer?.ToLower() == "да")
+        {
+        input = true;
+        }
+        else if (answer?.ToLower() == "нет")
+        {
+            Console.WriteLine($"Вы ввели чисел больше 0: {count}. Всего доброго!");
+            input = false;
+        }
+        else
+        {
+            Console.WriteLine("вы ввели ответ неправильно. Либо 'да' либо 'нет'!!!");
+            goto Repeat2;
+        }
+    }
+}
 
-// for (int i = 0; i < array.Length; i++)
-// {
-//     int index = i + 1;
-//     Console.Write($" Введите {index} элемент массива с индексом {i}: ");
-//     array[i] = Convert.ToInt32(Console.ReadLine());
-//     Console.WriteLine('[' + string.Join(", ", array) + "]");
-// }
 
-// Console.Write('[' + string.Join(", ", array) + "]");
-// Console.WriteLine();
 
-// Console.Write("Введите элементы(через пробел): ");
+// Console.Write("Введите числа через пробел: ");
 // int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 // int count = 0;
  
@@ -34,7 +66,8 @@
 //         count++;
 //     }
 // }
- 
-// Console.WriteLine($"Кол-во элементов > 0: {count}");
+
+// Console.WriteLine($"Количество элементов больше 0: {count}");
+
 
 
---
+++ dopka/Program.cs
@@ -0,0 +1,51 @@
+﻿// Напишите программу для подсчета количества цифр в числе с помощью рекурсии.
+// 12345 -> 5
+Console.WriteLine("введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int NumbersAmount(int number)
+{
+    if (number > 0)
+    {
+    return NumbersAmount(number / 10);
+    }
+    else
+    {
+        return NumbersAmount(number);
+    }
+}
+Console.WriteLine(NumbersAmount(number));
+
+
+// int Fibonacci(int n)
+// {
+//     if (n == 1 || n == 2) return 1;
+//     else return Fibonacci(n-1) + Fibonacci(n-2); // return потому что нам нужно вернуть значение
+// }
+
+// for(int i = 1; i < n - 1; i++)
+// {
+//     Console.WriteLine(Fibonacci(i));
+// }
+
+// принимает на вход число и выдает количество цифр
+// Console.WriteLine("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int count = 0;
+// int number1 = number;
+// while (number > 0)
+// { number1 /= 10;
+// count++;
+// }
+// Console.WriteLine($"количество цифр в числе {number} равно: {count} ");
+// Console.Write("Введите число ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int num1 = num;
+// int count = 0;
+
+// while(num > 0){
+//     num /= 10;
+//     count++;
+// }
+
+// Console.WriteLine($"Количество цифр в числе {num1} равно {count}");
